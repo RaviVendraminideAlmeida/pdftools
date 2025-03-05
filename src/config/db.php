a28dd8d8a8623a4c17b5338a7982a4d8fa825dba
RaviVendraminideAlmeida <ravivendramini@gmail.com>
2025-03-05 13:07:16 -0300

    project scaffolding

---
+++ src/config/db.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Config;
+
+use PDO, PDOException; 
+
+class Connection
+{
+    private string $dsn = 'pgsql:host=postgres;dbname=pdftools;user=postgres;password=postdba';
+
+    private PDO $pdo;
+
+    public function __construct()
+    {
+        try {
+            $this->pdo = new PDO($this->dsn);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public function getPDO()
+    {
+        return $this->pdo;
+    }
+
+}
+
---
+++ src/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pdftools</title>
+    <link rel="stylesheet" href="/public/css/bootstrap.min.css">
+</head>
+<body>
+
+    <main class="p-2">
+
+        <h1>Hello, PHP!</h1>
+
+    </main>
+
+
+    
+</body>
+<script src="/public/js/bootstrap.min.js"></script>
+<script src="/public/js/jquery-3.7.1.min.js"></script>
+<script>
+    
+    window.onload = function(){
+        alert("Welcome");
+    }
+
+
+</script>
+
+</html>
